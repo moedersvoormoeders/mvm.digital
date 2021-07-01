1ed26eb0946e88dce748fa869b8f66a318f7e538
Maartje Eyskens <maartje@eyskens.me>
2021-07-01 18:08:30 +0200

    RBAC based navigation content

--- src/_services/auth.service.js
+++ src/_services/auth.service.js
@@ -1,57 +1,115 @@
-import config from 'config';
-import { authHeader } from '../_helpers/auth-header';
+import config from "config";
+import { authHeader } from "../_helpers/auth-header";
+import * as minimatch from "minimatch";
+
+let permissionsCache = null;
 
 const login = (username, password) => {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
-    };
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  };
 
-    return fetch(`${config.apiUrl}/login`, requestOptions)
-        .then(handleResponse)
-        .then(user => {
-            if (user.token) {
-                localStorage.setItem('user', JSON.stringify(user));
-            }
+  return fetch(`${config.apiUrl}/login`, requestOptions)
+    .then(handleResponse)
+    .then((user) => {
+      if (user.token) {
+        localStorage.setItem("user", JSON.stringify(user));
+      }
 
-            return user;
-        });
-}
+      return user;
+    });
+};
 
 const logout = () => {
-    localStorage.removeItem('user');
-}
+  localStorage.removeItem("user");
+};
+
+const getRoles = () => {
+  const requestOptions = {
+    method: "GET",
+    headers: authHeader(),
+  };
+
+  return fetch(`${config.apiUrl}/v1/auth/whoami/roles`, requestOptions).then(
+    handleResponse
+  );
+};
+
+// verb, path
+const canIDo = async (needVerb, needPath) => {
+  // allow nothing if not logged in
+  if (!localStorage.key("user")) {
+    return false;
+  }
+
+  if (!permissionsCache) {
+    permissionsCache = await getRoles();
+  }
+
+  for (let role of permissionsCache) {
+    let isVerb = false;
+    let isPath = false;
+
+    for (let verb of role.verbs) {
+      if (verb.content === needVerb) {
+        isVerb = true;
+        break;
+      }
+    }
+
+    if (!isVerb) {
+      break;
+    }
+
+    for (let ep of role.endpoints) {
+      if (minimatch(needPath, ep.content, { matchBase: true })) {
+        isPath = true;
+        break;
+      }
+    }
+
+    if (isVerb && isPath) {
+      return true;
+    }
+  }
+
+  return false;
+};
 
 const check = () => {
-    const requestOptions = {
-        method: 'GET',
-        headers: authHeader()
-    };
+  const requestOptions = {
+    method: "GET",
+    headers: authHeader(),
+  };
 
-    return fetch(`${config.apiUrl}/v1/auth/check`, requestOptions).then(handleResponse);
-}
+  return fetch(`${config.apiUrl}/v1/auth/check`, requestOptions).then(
+    handleResponse
+  );
+};
 
 export const authService = {
-    login,
-    logout,
-    check,
+  login,
+  logout,
+  check,
+  canIDo,
 };
 
 const handleResponse = (response) => {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if (response.status === 401) {
-                // auto logout if 401 response returned from api
-                logout();
-                location.reload(true);
-            }
+  return response.text().then((text) => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      if (response.status === 401) {
+        // auto logout if 401 response returned from api
+        logout();
+        location.reload(true);
+      }
 
-            const error = (data && data.error) || response.statusText;
-            return Promise.reject(error);
-        }
+      const error = (data && data.error) || response.statusText;
+      return Promise.reject(error);
+    }
 
-        return data;
-    });
-}
+    return data;
+  });
+};
--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,27 +1,71 @@
 <template id="nav">
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Moeders voor Moeders <span v-if="apiURL == 'https://api.staging.mvm.digital'" class="badge badge-warning">Staging</span></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="#"
+        >Moeders voor Moeders
+        <span
+          v-if="apiURL == 'https://api.staging.mvm.digital'"
+          class="badge badge-warning"
+          >Staging</span
+        ></a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li :class="$route.name == 'home' ? 'nav-item active' : 'nav-item'">
-            <router-link class="nav-link" to="/home"><i class="far fa-home"></i> Home</router-link>
+            <router-link class="nav-link" to="/home"
+              ><i class="far fa-home"></i> Home</router-link
+            >
           </li>
-          <li :class="$route.name.indexOf('materiaal') == 0 ? 'nav-item active' : 'nav-item'">
-            <router-link class="nav-link" to="/materiaal/search"><i class="far fa-tshirt"></i> Materiaal</router-link>
+          <li
+            v-if="permissions.materiaal"
+            :class="
+              $route.name.indexOf('materiaal') == 0
+                ? 'nav-item active'
+                : 'nav-item'
+            "
+          >
+            <router-link class="nav-link" to="/materiaal/search"
+              ><i class="far fa-tshirt"></i> Materiaal</router-link
+            >
           </li>
-          <li :class="$route.name.indexOf('voeding') == 0 ? 'nav-item active' : 'nav-item'">
-            <router-link class="nav-link" to="/voeding/search"><i class="far fa-utensils"></i> Voeding</router-link>
+          <li
+            v-if="permissions.voeding"
+            :class="
+              $route.name.indexOf('voeding') == 0
+                ? 'nav-item active'
+                : 'nav-item'
+            "
+          >
+            <router-link class="nav-link" to="/voeding/search"
+              ><i class="far fa-utensils"></i> Voeding</router-link
+            >
           </li>
-          <li :class="$route.name.indexOf('sinterklaas') == 0 ? 'nav-item active' : 'nav-item'">
-            <router-link class="nav-link" to="/sinterklaas/search"><i class="far fa-staff"></i> Sinterklaas</router-link>
+          <li
+            v-if="permissions.sinterklaas"
+            :class="
+              $route.name.indexOf('sinterklaas') == 0
+                ? 'nav-item active'
+                : 'nav-item'
+            "
+          >
+            <router-link class="nav-link" to="/sinterklaas/search"
+              ><i class="far fa-staff"></i> Sinterklaas</router-link
+            >
           </li>
         </ul>
-       <!-- <form class="form-inline my-2 my-lg-0">
+        <!-- <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>-->
@@ -34,13 +78,35 @@
 </template>
 
 <script>
-import config from 'config';
+import config from "config";
+import { authService } from "../_services/auth.service";
 
 export default {
   data: function () {
     return {
       apiURL: config.apiUrl,
+      permissions: {
+        materiaal: false,
+        voeding: false,
+        sinterklaas: false,
+      },
     };
   },
-}
+  methods: {
+    canIDo: async function (verb, path) {
+      return await authService.canIDo(verb, path);
+    },
+  },
+  created() {
+    authService.canIDo("GET", "/v1/matariaal/klant").then((r) => {
+      this.permissions.materiaal = r;
+    });
+    authService.canIDo("GET", "/v1/voeding/klant").then((r) => {
+      this.permissions.voeding = r;
+    });
+    authService.canIDo("GET", "/v1/sinterklaas/klant").then((r) => {
+      this.permissions.sinterklaas = r;
+    });
+  },
+};
 </script>
