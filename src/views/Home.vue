c8eee4732c99a2ca01a1093988539803ae18576b
Maartje Eyskens <maartje@eyskens.me>
2021-07-15 12:49:42 +0200

    Add a voeding chart to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,24 +8,75 @@
 
     <div v-if="!loading">
       <h1>Home</h1>
+      <hr />
+      <h2>Statistiek</h2>
+      <div class="row">
+        <div class="col-6" v-if="permissions.voeding">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Voeding</h5>
+              <p class="card-text">Vandaag: {{ aantalVoeding }} paketten</p>
+              <voeding-chart :chartdata="voedingData"></voeding-chart>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { authService } from "../_services/auth.service"
+import { authService } from "../_services/auth.service";
+import VoedingChart from "./dashboard/voedingChart";
+import { dashboardService } from "@/_services/dashboard.service";
 
 export default {
   template: "#home",
-  components: {},
-  data: function() {
+  components: { VoedingChart },
+  data: function () {
     return {
       loading: false,
+      permissions: {
+        voeding: false,
+      },
+      voedingData: {
+        labels: [],
+        datasets: [],
+      },
+      aantalVoeding: 0,
     };
   },
 
-  created: function() {
-    authService.check().catch(() => this.$router.push("/login"))
-  }
+  methods: {
+    handleVoedingResponse: function (resp) {
+      this.voedingData = {
+        labels: Object.keys(resp),
+        datasets: [
+          {
+            label: "Paketten",
+            backgroundColor: "#695AD8",
+            data: Object.values(resp),
+          },
+        ],
+      };
+
+      for (let key in resp) {
+        if (key.substr(0, 10) == new Date().toISOString().substr(0, 10)) {
+          this.aantalVoeding = resp[key];
+          break;
+        }
+      }
+    },
+  },
+
+  created: function () {
+    authService.check().catch(() => this.$router.push("/login"));
+    authService.canIDo("GET", "/v1/dashboard/voeding").then((r) => {
+      this.permissions.voeding = r;
+      if (r) {
+        dashboardService.getVoeding().then(this.handleVoedingResponse);
+      }
+    });
+  },
 };
 </script>
---
+++ src/views/dashboard/voedingChart.vue
@@ -0,0 +1,37 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: ["chartdata"],
+  watch: {
+    chartdata() {
+      this.render();
+    },
+  },
+  methods: {
+    render: function () {
+      for (let i in this.chartdata.labels) {
+        this.chartdata.labels[i] = this.chartdata.labels[i].substr(0, 10);
+      }
+      this.renderChart(this.chartdata, {
+        responsive: true,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      });
+    },
+  },
+  mounted() {
+    this.render();
+  },
+};
+</script>
+
+<style></style>
